5f54986ef07f33ca6edc53b8bc661554f7bc6b4f
QuyTruong <truong.4725212@gmail.com>
2024-10-10 22:41:45 +0700

    OOP

--- java1_1.java
+++ java1_1.java
@@ -2,12 +2,12 @@ import btth1_java.*;
 import java.util.*;
 
 public class java1_1 {
-    public static void menu(){
-        for (int i = 1; i <= 5 ; i++)
-            System.out.println(i + ".Bai " + i);
-        System.out.println("===================");
-    }
+    
     public static void main(String[] args) {
         
+        System.out.println("Bai 1:\n");
+        bt1_1.DoanSo();
+        System.out.println("Bai 2:\n");
+        System.out.println("Ket qua la: " + bt2_1.GiaiPt());
     }
 }
